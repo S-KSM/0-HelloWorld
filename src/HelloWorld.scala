bfe9a15bc51d593520c0f028aeeb90ab5146bac5
Shobeir <shobeir.mazinani@gmail.com>
2016-11-26 10:29:45 -0700

    Initial commit

---
+++ src/HelloWorld.scala
@@ -0,0 +1,10 @@
+/**
+  * Created by Shobeir on 11/26/2016.
+  */
+object HelloWorld {
+
+  def main(args: Array[String]): Unit = {
+    print("""Hello World %s""".format("Does this work?"))
+  }
+
+}
